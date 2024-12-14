e7d075083bf1a6af0fdbb604968023bf96f1164e
Humanoid2005 <sriramsrikanth2005@gmail.com>
2024-12-14 18:57:03 +0530

    added basic files

--- Frontend/src/components/App.jsx
+++ Frontend/src/components/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import {Routes,Route,Link, BrowserRouter} from "react-router-dom";
 
 function App() {
-
+    return (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>}></Route>
+          </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
